49e61fcda8634696957c12457f7557f9d3a838f1
alexnbg <95033442+alexnbg@users.noreply.github.com>
2022-03-22 17:30:44 +0200

    Add files via upload

---
+++ voltdb_provider/__init__.py
@@ -0,0 +1,9 @@
+def get_provider_info():
+    return {
+        "package-name": "airflow-provider-voltdb",
+        "name": "VoltDB Airflow Provider",
+        "description": "VoltDB provider package for Apache Airflow.",
+        "hook-class-names": ["voltdb_provider.hooks.base_voltdb_hook.BaseVoltDBHook"],
+        # "extra-links": ["voltdb_provider.operators.sample_operator.ExtraLink"],
+        "versions": ["1.0.0"]
+    }
