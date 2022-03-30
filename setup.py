acaddb7af5eb7d3a9c162bfe747d88db0d98c383
alexnbg <95033442+alexnbg@users.noreply.github.com>
2022-03-30 17:31:50 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r") as readme:
     long_description = readme.read()
 
 setup(name='airflow-provider-voltdb',
-      version="1.0.2",
+      version="1.0.3",
       description='VoltDB provider package',
       long_description=long_description,
       long_description_content_type='text/markdown',
       entry_points={
           "apache_airflow_provider": [
-              "provider_info=voltdb_provider.__init__:get_provider_info"
+              "provider_info=voltdb_provider.get_provider_info:get_provider_info"
           ]
       },
       packages=[
-          'voltdb_provider', 'voltdb_provider.hooks',
-          #   'voltdb_provider.sensors', 'voltdb_provider.operators'
+          'voltdb_provider',
+          'voltdb_provider.hooks',
       ],
       install_requires=['apache-airflow>=2.0'],
       setup_requires=['setuptools', 'wheel'],
