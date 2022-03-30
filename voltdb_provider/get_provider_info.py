e1dfda77d164458c121bc7db7eac0ae877462221
alexnbg <95033442+alexnbg@users.noreply.github.com>
2022-03-30 17:37:09 +0300

    Update get_provider_info.py

--- voltdb_provider/get_provider_info.py
+++ voltdb_provider/get_provider_info.py
@@ -3,12 +3,25 @@ def get_provider_info():
         "package-name": "airflow-provider-voltdb",
         "name": "Airflow VoltDB Provider",
         "description": "Airflow provider for connection to VoltDB",
-        "versions": ["1.0.2"],
+        "versions": ["1.0.3"],
+        'integrations': [
+            {
+                'integration-name': 'VoltDB',
+                'external-doc-url': 'https://docs.voltdb.com/',
+                'tags': ['software'],
+            }
+        ],
+        'hooks': [
+            {
+                'integration-name': 'VoltDB',
+                'python-modules': ['voltdb_provider.hooks.base_voltdb_hook'],
+            }
+        ],
         "hook-class-names": ["voltdb_provider.hooks.base_voltdb_hook.BaseVoltDBHook"],
         "connection-types": [
             {
                 "hook-class-name": "voltdb_provider.hooks.base_voltdb_hook.BaseVoltDBHook",
-                "connection-type": "voltdb"
+                "connection-type": "voltdb",
             }
-        ]
+        ],
     }
