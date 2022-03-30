6e4928c9ca1184191bce28e5db24307d4bf99d01
alexnbg <95033442+alexnbg@users.noreply.github.com>
2022-03-30 17:32:33 +0300

    Update __init__.py

--- voltdb_provider/__init__.py
+++ voltdb_provider/__init__.py
@@ -1,17 +1,1 @@
-def get_provider_info():
-    return {
-        "package-name": "airflow-provider-voltdb",
-        "name": "VoltDB Airflow Provider",
-        "description": "VoltDB provider package for Apache Airflow.",
-        "hook-class-names": [
-            "voltdb_provider.hooks.base_voltdb_hook.BaseVoltDBHook"
-        ],
-        "connection-types": [
-            {
-                "connection-type": "voltdb",
-                "hook-class-name": "voltdb_provider.hooks.base_voltdb_hook.BaseVoltDBHook"
-            }
-        ],
-        # "extra-links": ["voltdb_provider.operators.sample_operator.ExtraLink"],
-        "versions": ["1.0.2"]
-    }
+
